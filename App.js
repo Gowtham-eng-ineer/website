b14241cc2e8dde586dc83c96791654eb4544fb2f
Gowtham-eng-ineer <143833428+Gowtham-eng-ineer@users.noreply.github.com>
2023-11-17 15:27:30 +0530

    website

---
+++ App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to My React App</h1>
+      </header>
+      <main>
+        <section>
+          <h2>About Us</h2>
+          <p>This is a simple React webpage created for demonstration purposes.</p>
+        </section>
+        <section>
+          <h2>Contact Us</h2>
+          <p>Feel free to contact us for more information.</p>
+        </section>
+      </main>
+      <footer>
+        <p>&copy; 2023 My React App</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
